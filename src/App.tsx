73a81affc0a9a7178e8d1dd65e08295e7d257df4
Ankur-v <dev.3ankurv@gmail.com>
2023-08-25 21:56:39 +0530

    Todo WebVitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactElement, useState } from 'react';
-
+// should add web-vitals todo
 import { ThemeProvider } from '@emotion/react';
 import { customTheme } from './theme/customTheme';
 import CssBaseline from '@mui/material/CssBaseline';
